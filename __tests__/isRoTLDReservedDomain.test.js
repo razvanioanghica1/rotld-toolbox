79fd5ef9bb2b41134aafc0b0157b990fca0d0329
Alin-Andrei Chican <alin@chican.ro>
2018-04-26 02:31:54 +0300

    Improve test isRoTLDReservedDomain

--- __tests__/isRoTLDReservedDomain.test.js
+++ __tests__/isRoTLDReservedDomain.test.js
@@ -1,22 +1,26 @@
 const isRoTLDReservedDomain = require(`../isRoTLDReservedDomain`);
-const RoTLDReservedDomains = require(`../RoTLDReservedDomains`);
+jest.mock('../RoTLDReservedDomains')
 
-test(`isRoTLDReservedDomain without argument`, () => {
+test(`throw without argument`, () => {
   expect(() => isRoTLDReservedDomain()).toThrow();
 });
 
-test(`isRoTLDReservedDomain with empty argument`, () => {
+test(`should throw on empty argument`, () => {
   expect(() => isRoTLDReservedDomain(``)).toThrow();
 });
 
-test(`isRoTLDReservedDomain with domain that doesn't have a romanian tld`, () => {
-  expect(isRoTLDReservedDomain(`subdomain.example.com`)).toBe(false);
+test(`should return false on domain that doesn't have a romanian tld`, () => {
+  expect(isRoTLDReservedDomain(`example.com`)).toBe(false);
 });
 
-test(`isRoTLDReservedDomain with domain that is a RoTLD reserved domain`, () => {
-  expect(isRoTLDReservedDomain(`${RoTLDReservedDomains[1]}.ro`)).toBe(true);
+test(`should return true on domain that is a RoTLD reserved domain`, () => {
+  expect(isRoTLDReservedDomain(`ș.ro`)).toBe(true);
 });
 
-test(`isRoTLDReservedDomain with domain that isn't a RoTLD reserved domain`, () => {
-  expect(isRoTLDReservedDomain(`example.ro`)).toBe(false);
+test(`should return true on domain that is a RoTLD reserved domain with RoTLD subdomain`, () => {
+  expect(isRoTLDReservedDomain(`ș.www.ro`)).toBe(true);
+});
+
+test(`should return false on domain that isn't a RoTLD reserved domain`, () => {
+  expect(isRoTLDReservedDomain(`s.ro`)).toBe(false);
 });
